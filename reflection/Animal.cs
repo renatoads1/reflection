22326d66600972287d884c0d10d0987c96eaf265
Renato de Azevedo Lacerda <renatoads1@gmail.com>
2025-09-22 13:44:36 -0300

    Adicionar arquivos de projeto.

---
+++ reflection/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reflection
+{
+    public class Animal
+    {
+        public string Som { get; set; }
+
+        public void EmitirSom(string som)
+        {
+            Som = som;
+            Console.WriteLine(Som);
+        }
+    }
+}
---
+++ reflection/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using reflection;
+
+Type tipo = typeof(Animal);
+object instancia = Activator.CreateInstance(tipo);
+var metodo = tipo.GetMethod("EmitirSom");
+metodo.Invoke(instancia, new object[] { "Au Au" });
